9dfae681c21022c4276e605e33f28ef33d732e9b
“rejownahmed” <rejownahmedzisan@gmail.com>
2023-12-09 15:16:10 +0600

    Comments added

--- src/formDataAssigner.js
+++ src/formDataAssigner.js
@@ -3,6 +3,24 @@
  * @github RejownAhmedZisan
 */
 
+/**
+ * FormData is not accessible in node version lower than 18
+ * So we need to Import FormData from 'form-data'
+ * However, newer version supports FormData by default no need to import
+*/
+
+// import FormData from "form-data";
+
+/**
+ * This is the main function which does the work
+ * Copy and paste in your project where you like
+ * And voila! You are ready to do anything with it :)
+ * @param {object} dataObject 
+ * @param {FormData} formData 
+ * @param {string} parentKey 
+ * @returns {FormData}
+ */
+
 //Use the below function in your project
 const formDataAssigner = (dataObject, formData = new FormData(), parentKey) => {
   //Validations
